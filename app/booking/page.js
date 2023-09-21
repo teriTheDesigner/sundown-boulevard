98bd5ce87f3ac20e2217c8649558124b0cdfd6ff
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-21 10:07:14 +0200

    Update page.js

--- app/booking/page.js
+++ app/booking/page.js
@@ -19,22 +19,21 @@ export default function Booking() {
     });
   }
 
-
   useEffect(() => {
-    const updatingOrderId = localStorage.getItem('updatingOrder');
+    const updatingOrderId = localStorage.getItem("updatingOrder");
     if (!updatingOrderId) {
-        dispatch({ type: "CLEAR_BASKET" });
+      dispatch({ type: "CLEAR_BASKET" });
     }
 
     const fetchSavedMeals = () => {
       if (!updatingOrderId) {
-          return []; //no meals if nor oderid
+        return []; //no meals if nor oderid
       }
 
       const savedOrder = JSON.parse(localStorage.getItem(updatingOrderId));
       if (savedOrder && savedOrder.meals) {
         const uniqueMealIds = new Set();
-        return savedOrder.meals.filter(meal => {
+        return savedOrder.meals.filter((meal) => {
           const notSeenBefore = !uniqueMealIds.has(meal.mealId);
           uniqueMealIds.add(meal.mealId);
           return notSeenBefore;
@@ -45,40 +44,40 @@ export default function Booking() {
 
     const savedMeals = fetchSavedMeals();
     const mealsToFetch = 9 - savedMeals.length;
-    fetchMeals(mealsToFetch).then(fetchedMeals => {
-        setMealData([...savedMeals, ...fetchedMeals]);
+    fetchMeals(mealsToFetch).then((fetchedMeals) => {
+      setMealData([...savedMeals, ...fetchedMeals]);
     });
-}, []);
+  }, []);
 
+  async function fetchMeals(count) {
+    const meals = [];
+    for (let i = 0; i < count; i++) {
+      const res = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/random.php",
+      );
+      const data = await res.json();
+      const allMeal = {
+        mealName: data.meals[0].strMeal,
+        mealId: data.meals[0].idMeal,
+        mealCategory: data.meals[0].strCategory,
+        mealImg: data.meals[0].strMealThumb,
+      };
+      meals.push(allMeal);
+    }
+    return meals;
+  }
 
-      async function fetchMeals(count) {
-        const meals = [];
-        for (let i = 0; i < count; i++) {
-          const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-          const data = await res.json();
-          const allMeal = {
-            mealName: data.meals[0].strMeal,
-            mealId: data.meals[0].idMeal,
-            mealCategory: data.meals[0].strCategory,
-            mealImg: data.meals[0].strMealThumb,
-          };
-          meals.push(allMeal);
-        }
-        return meals;
-      }
-
-      const selectMeal = (meal) => {
-        dispatch({
-          type: "ADD_MEAL",
-          payload: {
-            mealName: meal.mealName,
-            mealId: meal.mealId,
-            mealCategory: meal.mealCategory,
-            mealImg: meal.mealImg
-          },
-        });
-    };
-    
+  const selectMeal = (meal) => {
+    dispatch({
+      type: "ADD_MEAL",
+      payload: {
+        mealName: meal.mealName,
+        mealId: meal.mealId,
+        mealCategory: meal.mealCategory,
+        mealImg: meal.mealImg,
+      },
+    });
+  };
 
   return (
     <div className="content-container mx-auto pb-32 pt-16">
@@ -86,25 +85,40 @@ export default function Booking() {
 
       <div className="grid grid-cols-12 gap-2 pt-6">
         <div className="col-start-1 col-end-10 grid grid-cols-3 gap-4">
-        {mealData.map(meal => (
-        <div key={meal.mealId} className="flex flex-col items-center gap-4">
-          <Image
-            width="150"
-            height="150"
-            className="w-full"
-            src={meal.mealImg}
-            alt={meal.mealName}
-          />
-          <h2>{meal.mealName.toUpperCase()}</h2>
-          <p className="text-sm">Category: {meal.mealCategory}</p>
-          <button
-            onClick={() => selectMeal(meal)}
-            className="h-8 w-32 rounded-lg border-2 border-dark-blue bg-white text-xs text-dark-blue hover:bg-dark-blue hover:text-background-white"
-          >
-            SELECT
-          </button>
-        </div>
-      ))}
+          {mealData.map((meal) => {
+            const mealCount = customer.meals.filter(
+              (chosenMeal) => chosenMeal.mealId === meal.mealId,
+            ).length;
+            const isSelected = mealCount > 0;
+
+            return (
+              <div
+                onClick={() => selectMeal(meal)}
+                key={meal.mealId}
+                className={`relative flex flex-col items-center gap-4 rounded-lg pb-2 text-center 
+        ${
+          isSelected
+            ? "border-2 border-dark-purple/80"
+            : "border-2 border-light-blue"
+        }`}
+              >
+                {isSelected && (
+                  <div className="absolute right-2 top-2 flex h-5 w-5 items-center justify-center rounded-full bg-dark-purple text-white">
+                    {mealCount}
+                  </div>
+                )}
+                <Image
+                  width="150"
+                  height="150"
+                  className="w-full"
+                  src={meal.mealImg}
+                  alt={meal.mealName}
+                />
+                <h2>{meal.mealName.toUpperCase()}</h2>
+                <p className="text-sm">Category: {meal.mealCategory}</p>
+              </div>
+            );
+          })}
         </div>
 
         <div className="top-1/5 sticky col-start-11 col-end-13 flex h-96 flex-col gap-4 border-l border-dark-purple pl-4 text-sm">
